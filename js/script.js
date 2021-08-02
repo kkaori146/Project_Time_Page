da07eb7dd802b0c330839bf18f4aec6a6f93873b
Kaminishikawahara KK <kkaori146@gmail.com>
2021-08-02 10:57:56 +0900

    Working with images inside JavaScript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-alert('Olá')
+function carregar() {
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+//var hora = data.getHours()
+
+var hora = 19
+
+msg.innerHTML = `Agora são ${hora} horas.`
+
+if (hora >= 0 && hora <12) {
+//Good Morning!
+img.src = "images/manha.png"
+document.body.style.background = '#e2cd9f'
+
+}  else if (hora >= 12 && hora <= 18) {
+//Good Afternoon!
+img.src = "images/tarde.png"
+document.body.style.background = '#b9846f'
+
+} else {
+    //Good Evening!
+    img.src = "images/noite.png"
+    document.body.style.background = '#515154'
+}
+}
